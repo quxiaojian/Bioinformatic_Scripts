039d94c5e0ab737523e85ad6dddb550270dce29d
Xiaojian Qu <quxiaojian@mail.kib.ac.cn>
2017-07-16 18:17:05 +0800

    perl scripts and examples

--- parse_one_file_into_multiple_files_based_on_genename/parse_one_file_into_multiple_files_based_on_genename.pl
+++ parse_one_file_into_multiple_files_based_on_genename/parse_one_file_into_multiple_files_based_on_genename.pl
@@ -1,32 +1,11 @@
 #!/usr/bin/perl
 use strict;
-use Term::ProgressBar;
 
 open (my $genename,"<","genename.txt");
 chomp (my @names=<$genename>);
 close $genename;
 
 open (my $file,"<","PCG_M.fasta");
-my $seqcount = 0;
-while (<$file>) {
-	$seqcount++ if(/^>/);
-}
-my $progress=Term::ProgressBar->new({
-	count		=>	$seqcount,
-	name		=>	'Processing',
-	major_char	=>	'=',			# default symbol of major progress bar
-	minor_char	=>	'*',			# default symbol of minor progress bar
-	ETA			=>	'linear',		# evaluate remain time: undef (default) or linear
-	#term_width	=>	100,			# breadth of terminal, full screen (default)
-	#remove		=>	0,				# whether the progress bar disappear after the end of this script or not? 0 (default) or 1
-	#fh			=>	\*STDOUT,		# \*STDERR || \*STDOUT
-});
-$progress->lbrack('[');				# left symbol of progress bar
-$progress->rbrack(']');				# right symbol of progress bar
-$progress->minor(0);				# close minor progress bar
-#$progress->max_update_rate(0.5);	# minumum gap time between two updates (s)
-
-
 my $outdir="output";
 system ("rm -rf output") if (-e $outdir);
 system ("mkdir output") if (! -e $outdir);
@@ -42,8 +21,5 @@ while (chomp (my $header=<$file>) && chomp (my $sequence=<$file>)) {
 			close $fh;
 		}
 	}
-	$count++;
-	$update=$progress->update ($count) if ($count > $update);
 }
-$progress->update ($seqcount) if ($seqcount >= $update);
 close $file;
