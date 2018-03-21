9efb1da78874a9fb23061da1acc07f622ebe2694
Xiao-Jian Qu <quxiaojian@mail.kib.ac.cn>
2018-03-21 23:17:59 +0800

    update extract_polymorphic_and_constant_sites.pl

--- extract_polymorphic_and_constant_sites/extract_polymorphic_and_constant_sites.pl
+++ extract_polymorphic_and_constant_sites/extract_polymorphic_and_constant_sites.pl
@@ -1,18 +1,24 @@
 #!/usr/bin/perl -w
 use strict;
+use Getopt::Long;
 use Data::Dumper;
+$|=1;
 #array of hash plus transposed matrix(two-dimensional array)
 
-my $input_file="input.fasta";
-my $output_file1="site_polymorphic.fasta";
-my $output_file2="stie_constant.fasta";
+my $global_options=&argument();
+my $input_file=&default("input","input");
+my $output_file1=&default("output1","output1");
+my $output_file2=&default("output2","output2");
+
 open(my $input,"<",$input_file);
 open(my $output1,">",$output_file1);
 open(my $output2,">",$output_file2);
 
 my ($header,$sequence,$length,@id,@site);
 while (defined ($header=<$input>) && defined ($sequence=<$input>)) {
-	chomp ($header,$sequence);
+	#chomp ($header,$sequence);
+	$header=~ s/\r|\n//g;
+	$sequence=~ s/\r|\n//g;
 	$length=length $sequence;
 	push @id,$header;
 
@@ -60,3 +66,72 @@ for (0..(@id-1)){
 close $input;
 close $output1;
 close $output2;
+
+
+##function
+sub argument{
+	my @options=("help|h","input|i:s","output1|o1:s","output2|o2:s");
+	my %options;
+	GetOptions(\%options,@options);
+	exec ("pod2usage $0") if ((keys %options)==0 or $options{'h'} or $options{'help'});
+	if(!exists $options{'input'}){
+		print "***ERROR: No input filename is assigned!!!\n";
+		exec ("pod2usage $0");
+	}elsif(!exists $options{'output1'}){
+		print "***ERROR: No output1 filename is assigned!!!\n";
+		exec ("pod2usage $0");
+	}elsif(!exists $options{'output2'}){
+		print "***ERROR: No output2 filename is assigned!!!\n";
+		exec ("pod2usage $0");
+	}
+	return \%options;
+}
+
+sub default{
+	my ($default_value,$option)=@_;
+	if(exists $global_options->{$option}){
+		return $global_options->{$option};
+	}
+	return $default_value;
+}
+
+
+__DATA__
+
+=head1 NAME
+
+    extract_polymorphic_and_constant_sites.pl
+
+=head1 COPYRIGHT
+
+    copyright (C) 2018 Xiao-Jian Qu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=head1 DESCRIPTION
+
+    extract polymorphic and constant sites
+
+=head1 SYNOPSIS
+
+    extract_polymorphic_and_constant_sites.pl -i -o1 -o2
+    Copyright (C) 2018 Xiao-Jian Qu
+    Please contact <quxiaojian@mail.kib.ac.cn>, if you have any bugs or questions.
+
+    [-h -help]         help information.
+    [-i -input]        required: (default: input.fasta) input filename of matrix.
+    [-o1 -output1]     required: (default: polymorphic.fasta) output1 filename of polymorphic sites matrix.
+    [-o2 -output2]     required: (default: constant.fasta) output2 filename of constant sites matrix.
+
+=cut
